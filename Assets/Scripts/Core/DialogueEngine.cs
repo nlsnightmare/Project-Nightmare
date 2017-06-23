bd80642ab5772b860bdff3d92870b0d4ad2f35d7
Νίκος Μαρουλάκης <nlsnightmare@gmail.com>
2017-06-24 00:44:53 +0300

    Started working on DialogueEngine

--- Assets/Scripts/Core/DialogueEngine.cs
+++ Assets/Scripts/Core/DialogueEngine.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,9 +10,9 @@ public class DialogueEngine : MonoBehaviour {
     public Text DialogueBox;
     public static DialogueEngine Instance;
     void Start(){
-	if(Instance == null){
+	if(Instance == null)
 	    Instance = this;
-	}
+	DialogueBox = GameObject.Find("DialogueBox").GetComponent<Text>();
     }
 
     public static void Print(string message){
@@ -22,4 +23,47 @@ public class DialogueEngine : MonoBehaviour {
     public static void Print(string[] messages){
 
     }
+
+
+
+    static Thread t;
+    static bool isReady = false;
+    static string StringToShow = "";
+    static string TargetString = "";
+    static int Delay;
+
+    static void ShowDialogue()
+    {
+        Player.SetState(Player.ControlState.Talking);
+        StringToShow = "";
+        foreach (var c in TargetString)
+        {
+            isReady = false;
+            StringToShow += c;
+            isReady = true;
+            Thread.Sleep(Delay);
+        }
+    }
+
+
+    void Update()
+    {
+        if (!isReady || !t.IsAlive) return;
+        DialogueBox.text = StringToShow;
+    }
+
+    public static void ShowText(string s, int delay)
+    {
+        Instance.DialogueBox.text = "";
+        TargetString = s;
+        Debug.Log(t);
+        if (t != null)
+        {
+            t.Abort();
+        }
+        t = new Thread(ShowDialogue);
+        Delay = delay;
+        t.Start();
+    }
+
 }
